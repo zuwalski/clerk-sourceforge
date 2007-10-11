07b250da5177ff8eb0b89826ef54138725dc1daa
szuwalski <szuwalski@a4423af8-e61d-0410-8e09-fb0f2c69f8c8>
2007-10-11 21:21:38 +0000

    git-svn-id: http://svn.code.sf.net/p/cle/code@75 a4423af8-e61d-0410-8e09-fb0f2c69f8c8

---
+++ clerk/trunk/cle_test.h
@@ -0,0 +1,26 @@
+/* 
+   Copyright 2005-2007 Lars Szuwalski
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#ifndef __CLE_TEST_H__
+#define __CLE_TEST_H__
+
+#include "cle_clerk.h"
+
+void st_prt_page(st_ptr* pt);
+
+int rt_do_read(st_ptr root);
+
+
+#endif
