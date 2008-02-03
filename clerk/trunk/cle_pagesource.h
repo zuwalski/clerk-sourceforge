2540e9f6fadfc9fb6b2e41c631f6f8f1b3e49f84
szuwalski <szuwalski@a4423af8-e61d-0410-8e09-fb0f2c69f8c8>
2008-02-03 22:34:30 +0000

    new license LGPLv3
    git-svn-id: http://svn.code.sf.net/p/cle/code@83 a4423af8-e61d-0410-8e09-fb0f2c69f8c8

---
+++ clerk/trunk/cle_pagesource.h
@@ -0,0 +1,33 @@
+/* 
+    Clerk application and storage engine.
+    Copyright (C) 2008  Lars Szuwalski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __CLE_PAGESOURCE_H__
+#define __CLE_PAGESOURCE_H__
+
+typedef void* cle_pagedata;
+typedef void* cle_pageid;
+typedef void* cle_psrc_data;
+
+typedef struct cle_pagesource
+{
+	cle_pagedata (*read_page)(cle_psrc_data, cle_pageid);
+	int (*lock_page)(cle_psrc_data, cle_pageid);
+	int (*write_page)(cle_psrc_data, cle_pageid, cle_pagedata, int length);
+}
+cle_pagesource;
+
+#endif
