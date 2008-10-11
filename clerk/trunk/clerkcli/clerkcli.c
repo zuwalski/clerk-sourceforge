3209a464e378a145082b0de6f59f446d310b3209
szuwalski <szuwalski@a4423af8-e61d-0410-8e09-fb0f2c69f8c8>
2008-10-11 00:29:28 +0000

    git-svn-id: http://svn.code.sf.net/p/cle/code@106 a4423af8-e61d-0410-8e09-fb0f2c69f8c8

---
+++ clerk/trunk/clerkcli/clerkcli.c
@@ -0,0 +1,23 @@
+/*
+	TEST-SUITE RUNNER
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../cle_core/cle_clerk.h"
+
+void unimplm()
+{
+	puts("failed in unimpl in clerkcli.c");
+	exit(-1);
+}
+
+uint page_size;
+uint resize_count;
+uint overflow_size;
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
